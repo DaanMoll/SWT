1df8a3d3da224f09d5a7a384d9efb40c2048aec5
adamimaria <34209781+adamimaria@users.noreply.github.com>
2021-04-19 15:47:07 +0300

    Update main.py

--- heartmonitor/main.py
+++ heartmonitor/main.py
@@ -46,6 +46,8 @@ def blood_pressure(number):
         print("WARNING! Low bloodpressure! seek medical attention!")
     elif first >= 140 and second >= 90:
         print("WARNING! High bloodpressure! Seek medical attention!")
+    elif first >= 140 and second <= 90 and second >= 60:
+        print("Isolated systolic hypertension! Normal for older people, but further tests should be done.")
     else:
         print("Blood pressure is normal")
     pass
