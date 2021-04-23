364d83edbac2095c805ca18078f2a9ec81cf01a2
Dmitry Telenkov <31540432+telenkov@users.noreply.github.com>
2021-04-23 18:12:55 +0200

    Update LegoCar.m

--- SYS/MATLAB/LegoCar.m
+++ SYS/MATLAB/LegoCar.m
@@ -8,6 +8,7 @@ classdef LegoCar < handle
         RightMulti;
         
         myrobot;
+        sonic;
         m_left;
         m_right;
         cs_left;
@@ -34,6 +35,8 @@ classdef LegoCar < handle
 
             obj.m_left = motor(obj.myrobot, 'B');
             obj.m_right = motor(obj.myrobot, 'C');
+            
+            obj.sonic = sonicSensor(obj.myrobot, 2);
 
             obj.cs_left = colorSensor(obj.myrobot,1);
             obj.cs_right = colorSensor(obj.myrobot,4);
@@ -54,8 +57,13 @@ classdef LegoCar < handle
             disp(obj.Speed);
         end
         
+        function Start(obj)
+            obj.m_left.start();
+            obj.m_right.start();
+        end
+        
         function UpdateMotorSpeed(obj)
-            disp(obj.Speed);
+%             disp(obj.Speed);
 %             disp(obj.LeftMulti);
 %             disp(obj.Speed * obj.LeftMulti);
 %             disp(obj.RightMulti);
@@ -65,23 +73,33 @@ classdef LegoCar < handle
         end
         
         function TurnWheelsLeft(obj)
+            obj.LeftMulti = -0.6 * obj.NormalMulti;
+            obj.RightMulti = obj.NormalMulti;
+        end
+        
+        function InplaceTurnWheelsLeft(obj)
             obj.LeftMulti = -obj.NormalMulti;
             obj.RightMulti = obj.NormalMulti;
         end
         
         function SharpTurnWheelsLeft(obj)
-            obj.LeftMulti = -obj.NormalMulti;
-            obj.RightMulti =  obj.SharpTurnMulti;
+            obj.LeftMulti = -0.6 * obj.NormalMulti;
+            obj.RightMulti =  obj.NormalMulti;
         end
         
         function TurnWheelsRight(obj)
-            obj.LeftMulti = obj.TurnMulti;
+            obj.LeftMulti = obj.NormalMulti;
+            obj.RightMulti = -0.6 * obj.NormalMulti;
+        end
+        
+        function InplaceTurnWheelsRight(obj)
+            obj.LeftMulti = obj.NormalMulti;
             obj.RightMulti = -obj.NormalMulti;
         end
         
         function SharpTurnWheelsRight(obj)
-            obj.LeftMulti = obj.SharpTurnMulti;
-            obj.RightMulti = -obj.NormalMulti;
+            obj.LeftMulti = obj.NormalMulti;
+            obj.RightMulti = -0.6 * obj.NormalMulti;
         end
         
         function TurnWheelsForward(obj)
@@ -99,8 +117,7 @@ classdef LegoCar < handle
         end
         
         function DriveUntilParking(obj)
-            obj.m_left.start();
-            obj.m_right.start();
+            obj.Start();
             
             while true
                 int_left = obj.cs_left.readLightIntensity('reflected');
@@ -140,6 +157,240 @@ classdef LegoCar < handle
                 end
             end
         end
+        
+        function DriveUntilDoubleBlack(obj)
+            obj.Start();
+            
+            while true
+                int_left = obj.cs_left.readLightIntensity('reflected');
+                int_right = obj.cs_right.readLightIntensity('reflected');
+
+                if CheckBothColorsLowerThenBlack(int_left, int_right)
+                    obj.Stop();
+                    break;
+                end
+                
+                speed_color = GetSpeedColor(int_left, int_right);
+                obj.SetSpeed(speed_color);
+                disp(obj.Speed);
+                obj.UpdateMotorSpeed();
+                
+                if obj.Turning == TurnDirection.Left
+                    if CheckColorLowerThenBlack(int_left)
+                        continue
+                    end
+                    obj.Turning = TurnDirection.Forward;
+                    obj.TurnWheelsForward();
+                end
+                if obj.Turning == TurnDirection.Right
+                    if CheckColorLowerThenBlack(int_right)
+                        continue;
+                    end
+                    obj.Turning = TurnDirection.Forward;
+                    obj.TurnWheelsForward();
+                end
+                if CheckColorLowerThenBlack(int_left)
+                    obj.Turning = TurnDirection.Left;
+                    obj.TurnWheelsLeft();
+                    continue;
+                elseif CheckColorLowerThenBlack(int_right)
+                    obj.Turning = TurnDirection.Right;
+                    obj.TurnWheelsRight();
+                    continue;
+                end
+            end
+        end
+        
+        function DriveUntilParkingTurns(obj)
+            obj.m_left.start();
+            obj.m_right.start();
+            
+            while true
+                int_left = obj.cs_left.readLightIntensity('reflected');
+                int_right = obj.cs_right.readLightIntensity('reflected');
+
+%                 if CheckBothColorsLowerThenBlack(int_left, int_right)
+%                     obj.StartSharpTurn();
+%                 end
+                
+                speed_color = GetSpeedColor(int_left, int_right);
+                obj.SetSpeed(speed_color);
+                disp(obj.Speed);
+                obj.UpdateMotorSpeed();
+                
+                if obj.Turning == TurnDirection.Left
+                    if CheckColorHigherThenBlack(int_right)
+                        continue
+                    end
+                    obj.Turning = TurnDirection.Forward;
+                    obj.TurnWheelsForward();
+                end
+                if obj.Turning == TurnDirection.Right
+                    if CheckColorHigherThenBlack(int_left)
+                        continue;
+                    end
+                    obj.Turning = TurnDirection.Forward;
+                    obj.TurnWheelsForward();
+                end
+                if CheckColorLowerThenBlack(int_left)
+                    obj.Turning = TurnDirection.Left;
+                    obj.TurnWheelsLeft();
+                    continue;
+                elseif CheckColorLowerThenBlack(int_right)
+                    obj.Turning = TurnDirection.Right;
+                    obj.TurnWheelsRight();
+                    continue;
+                end
+            end
+        end
+        
+        function TurnToLeftLine(obj)
+            obj.InplaceTurnWheelsLeft();
+            obj.SetSpeed(1);
+            obj.UpdateMotorSpeed();
+            obj.Start();
+            
+            int_right = obj.cs_right.readLightIntensity('reflected');
+            while CheckColorHigherThenBlack(int_right)
+                int_right = obj.cs_right.readLightIntensity('reflected');
+            end
+            
+            int_left = obj.cs_left.readLightIntensity('reflected');
+            int_right = obj.cs_right.readLightIntensity('reflected');
+            while CheckColorLowerThenBlack(int_right) || CheckColorLowerThenBlack(int_left)
+                int_left = obj.cs_left.readLightIntensity('reflected');
+                int_right = obj.cs_right.readLightIntensity('reflected');
+            end
+            
+            int_right = obj.cs_right.readLightIntensity('reflected');
+            while CheckColorHigherThenBlack(int_right)
+                int_right = obj.cs_right.readLightIntensity('reflected');
+            end
+            
+            obj.Stop();
+        end
+        
+        function CheckParkDirection(obj)
+            pause(3);
+            
+            obj.InplaceTurnWheelsLeft();
+            obj.SetSpeed(1);
+            obj.UpdateMotorSpeed();
+            
+            obj.Start();
+            
+            left_dist = 0;
+            left_times = 0;
+            
+            int_left = obj.cs_left.readLightIntensity('reflected');
+            int_right = obj.cs_right.readLightIntensity('reflected');
+            while CheckColorLowerThenBlack(int_right) || CheckColorLowerThenBlack(int_left)
+                left_dist = left_dist + obj.sonic.readDistance();
+                left_times = left_times + 1;
+                int_left = obj.cs_left.readLightIntensity('reflected');
+                int_right = obj.cs_right.readLightIntensity('reflected');
+            end
+            
+            int_right = obj.cs_right.readLightIntensity('reflected');
+            while CheckColorHigherThenBlack(int_right)
+                left_dist = left_dist + obj.sonic.readDistance();
+                left_times = left_times + 1;
+                int_right = obj.cs_right.readLightIntensity('reflected');
+            end
+            
+            obj.Stop();
+            pause(2);
+            
+            obj.InplaceTurnWheelsRight();
+            obj.UpdateMotorSpeed();
+            
+            obj.Start();
+            
+            int_left = obj.cs_left.readLightIntensity('reflected');
+            while CheckColorHigherThenBlack(int_left)
+                int_left = obj.cs_left.readLightIntensity('reflected');
+            end
+            
+            obj.Stop();
+            pause(3);
+            obj.Start();
+            
+            right_dist = 0;
+            right_times = 0;
+            
+            int_left = obj.cs_left.readLightIntensity('reflected');
+            int_right = obj.cs_right.readLightIntensity('reflected');
+            while CheckColorLowerThenBlack(int_right) || CheckColorLowerThenBlack(int_left)
+                right_dist = right_dist + obj.sonic.readDistance();
+                right_times = right_times + 1;
+                int_left = obj.cs_left.readLightIntensity('reflected');
+                int_right = obj.cs_right.readLightIntensity('reflected');
+            end
+            
+            int_left = obj.cs_left.readLightIntensity('reflected');
+            while CheckColorHigherThenBlack(int_left)
+                right_dist = right_dist + obj.sonic.readDistance();
+                right_times = right_times + 1;
+                int_left = obj.cs_left.readLightIntensity('reflected');
+            end
+            
+            obj.Stop();
+            
+            if ((right_dist / right_times) > (left_dist / left_times))
+                disp("right");
+            else
+                disp("left");
+                obj.TurnToLeftLine();
+            end
+        end
+        
+        function Park(obj)
+            obj.m_left.start();
+            obj.m_right.start();
+            
+            obj.SetSpeed(3)
+            obj.UpdateMotorSpeed();
+            
+            while true
+                obj.UpdateMotorSpeed();
+                int_left = obj.cs_left.readLightIntensity('reflected');
+                int_right = obj.cs_right.readLightIntensity('reflected');
+
+                if CheckBothColorsLowerThenBlack(int_left, int_right)
+                    obj.Stop();
+                    obj.CheckParkDirection();
+                    obj.Turning = TurnDirection.Forward;
+                    obj.DriveUntilDoubleBlack();
+                    break;
+                end
+                
+                if obj.Turning == TurnDirection.Left
+                    if CheckColorHigherThenBlack(int_right)
+                        continue
+                    end
+                    obj.Turning = TurnDirection.Forward;
+                    obj.TurnWheelsForward();
+                end
+                if obj.Turning == TurnDirection.Right
+                    if CheckColorHigherThenBlack(int_left)
+                        continue;
+                    end
+                    obj.Turning = TurnDirection.Forward;
+                    obj.TurnWheelsForward();
+                end
+                if CheckColorLowerThenBlack(int_left)
+                    obj.Turning = TurnDirection.Left;
+                    obj.TurnWheelsLeft();
+                    continue;
+                elseif CheckColorLowerThenBlack(int_right)
+                    obj.Turning = TurnDirection.Right;
+                    obj.TurnWheelsRight();
+                    continue;
+                end
+            end
+        end
+        
+        
         function Stop(obj)
             obj.m_left.stop();
             obj.m_right.stop();
@@ -159,6 +410,10 @@ end
 function res = CheckColorLowerThenBlack(intensity)
     res = intensity <= DefaultMaxIntensities.Black;
 end
+function res = CheckColorHigherThenBlack(intensity)
+    res = intensity > DefaultMaxIntensities.Black;
+end
+
 function res = CheckBothColorsLowerThenBlack(intensity1, intensity2)
     res = CheckColorLowerThenBlack(intensity1) && CheckColorLowerThenBlack(intensity2);
 end
